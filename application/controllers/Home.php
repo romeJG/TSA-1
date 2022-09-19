295386067406664d23c54051d68507146ca8fb0d
jastineee <201810083@fit.edu.ph>
2022-09-19 23:38:05 +0800

    Mario and Honey Comb are now working.

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -12,6 +12,69 @@ class Home extends CI_Controller
     public function index()
     {
         //tatawagin si home_view.php from the views folder and will load it on the page
+        $this->load->helper('url'); //for loading the css
         $this->load->view('home_view');
     }
+
+    //Problem number 1
+    public function MarioFlagPole($argu)
+    {
+        echo '<body style="    display: flex; justify-content: center;align-items: center;height: 100vh;background-color: rgb(110, 110, 110);;text-align: center;">';
+        echo '<div class="my_class" style="text-align: right;">'; // moving to the right to match the given output 
+        echo "<pre> Argument is equal to: " . $argu . " </pre>"; //printing the argument in the URI
+
+        $count = 1; // initialize to start in 1
+        for ($i = 1; $i <= $argu; $i++) { // the number of lines will depend on the input value of argument
+            for ($x = 0; $x <= $i; $x++) {
+                echo $count . " ";
+                $count++;
+            }
+            echo '<br/>';
+        }
+        echo '</div>';
+    }
+
+    //Problem number 2
+    public function BoxedFrame($argu1, $argu2)
+    {
+    }
+
+
+    //Problem number 3
+    public function HoneyComb($argu1, $argu2)
+    {
+        echo "<pre> Arg1 is equals to:" . $argu1 . "</pre>"; //printing the argument in the URI
+        echo "<pre> Arg2 is equals to:" . $argu2 . " </pre>"; //printing the argument inthe URI
+
+
+        for ($a = 1; $a < 7 * $argu1; $a++) {
+            for ($b = 1; $b < 7 * $argu2; $b++) {
+                if ($a == 1 || $a == 6 || $a == 11) {
+                    if ($b != 3 && $b != 4 && $b != 8 && $b != 9) {
+                        echo "&nbsp";
+                    } else {
+                        echo "*";
+                    }
+                }
+                if ($a == 2 || $a == 5 || $a == 7 || $a == 10) {
+                    if ($b != 2 && $b != 5 && $b !== 7 && $b != 10) {
+                        echo "&nbsp";
+                    } else {
+                        echo "*";
+                    }
+                }
+                if ($a == 3 || $a == 4 || $a == 8 || $a == 9) {
+                    if ($b != 1 && $b != 6 && $b != 12) {
+                        echo "&nbsp";
+                    } else {
+                        echo "*";
+                    }
+                }
+            }
+            echo "<br>";
+        }
+    }
 }
+
+//Code by Justine Rome M. Guillermo
+//code from: https://github.com/romeJG/TSA-1
--- application/views/home_view.php
+++ application/views/home_view.php
@@ -5,15 +5,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>css/home.css">
     <title>Summative Assesment 1</title>
 </head>
 
 <body>
-    <ul style="list-style-type: none; margin-bottom:10px;">
-        <li><a href="">Mario Tower</a></li>
-        <li><a href="">Boxed Frame</a></li>
-        <li><a href="">Honey Comb</a></li>
-    </ul>
+
+
+    <div id="container">
+        <h1>TSA 1</h1>
+        <ul id="navlist">
+            <li><a href="<?= base_url('Home/MarioFlagPole/5') ?>">Mario Tower</a></li>
+            <li><a href="<?= base_url('Home/BoxedFrame/5/5') ?>">Boxed Frame</a></li>
+            <li><a href="<?= base_url('Home/HoneyComb/5/5') ?>">Honey Comb</a></li>
+        </ul>
+    </div>
 </body>
 
 </html>
